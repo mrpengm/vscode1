e3c6d614635b0bc73c94a62dced465b875f0a0a0
max <mrpengmforxmail.com>
2020-09-02 20:53:02 +0800

    头像上传

--- src/views/edu/teacher/save.vue
+++ src/views/edu/teacher/save.vue
@@ -25,6 +25,31 @@
         <el-input v-model="teacher.intro" :rows="10" type="textarea"/>
       </el-form-item>
       <!-- 讲师头像：TODO -->
+      <!-- 讲师头像 -->
+      <el-form-item label="讲师头像">
+          <!-- 头衔缩略图 -->
+          <pan-thumb :image="teacher.avatar"/>
+          <!-- 文件上传按钮 -->
+          <el-button type="primary" icon="el-icon-upload" @click="imagecropperShow=true">更换头像
+          </el-button>
+          <!--
+              v-show：是否显示上传组件
+              :key：类似于id，如果一个页面多个图片上传控件，可以做区分
+              :url：后台上传的url地址
+              @close：关闭上传组件
+              @crop-upload-success：上传成功后的回调 -->
+          <image-cropper
+              v-show="imagecropperShow"
+              :width="300"
+              :height="300"
+              :key="imagecropperKey"
+              :url="BASE_API+'/eduoss/fileoss'"
+              field="file"
+              @close="close"
+              @crop-upload-success="cropSuccess"/>
+      </el-form-item>
+
+
       <el-form-item>
         <el-button :disabled="saveBtnDisabled" type="primary" @click="saveOrUpdate">保存</el-button>
       </el-form-item>
@@ -34,7 +59,10 @@
 
 <script>
 import teacherApi from '@/api/edu/teacher'
+import ImageCropper from '@/components/ImageCropper'
+import PanThumb from '@/components/PanThumb'
 export default {
+    components: { ImageCropper, PanThumb },
     data(){
         return{
             teacher:{
@@ -45,6 +73,10 @@ export default {
                 intro: '',
                 avatar: ''
             },
+            //上传弹框组件是否显示
+            imagecropperShow:false,
+            imagecropperKey:0,//上传组件key的标识，默认为0
+            BASE_API:process.env.BASE_API,//获取dev.env.js里面的地址和端口号
             saveBtnDisabled:false //保存按钮是否禁用，防止二次提交
         }
     },
@@ -57,6 +89,15 @@ export default {
       }
     },
     methods:{
+      close(){//关闭上传弹框的方法
+        this.imagecropperShow = false
+        this.imagecropperKey = this.imagecropperKey + 1
+      },
+      cropSuccess(data){//上传成功的方法
+        this.imagecropperShow = false
+        this.teacher.avatar = data.url
+        this.imagecropperKey = this.imagecropperKey + 1
+      },
       init(){
               //判断路径是否有id值
           if(this.$route.params && this.$route.params.id){
